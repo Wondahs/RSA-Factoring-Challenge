2e2548ed958d13eccf520394d42d8da7a3527fe4
Wonders <wondersprince1@gmail.com>
2023-10-22 03:54:35 -0700

    C in python

--- python/factors.py
+++ python/factors.py
@@ -6,6 +6,8 @@ import os
 
 if "LD_LIBRARY_PATH" not in os.environ:
   os.environ["LD_LIBRARY_PATH"] = os.environ["PWD"]
+else:
+  os.environ["LD_LIBRARY_PATH"] += ":."
 
 def is_factor(num, factor):
     if (num % factor == 0):
